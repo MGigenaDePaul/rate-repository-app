90101e99faf7c3d96cb19832ca2c4b81a3ec837b
Miqueas <miqueasgigena18@gmail.com>
2025-11-26 15:57:57 -0300

    Exercise 10.4

---
+++ rate-repository-app/src/components/AppBar.jsx
@@ -0,0 +1,27 @@
+import { View, Text, StyleSheet, Pressable } from 'react-native';
+import Constants from 'expo-constants';
+import theme from '../theme';
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: theme.backgroundColors.appBar,
+  }, 
+  tabText: {
+    color: theme.colors.textPrimary,
+    fontSize: theme.fontSizes.subheading,
+    paddingHorizontal: 10,
+  }
+});
+
+const AppBar = () => {
+  return (
+    <View style={styles.container}>
+        <Pressable>
+            <Text style={styles.tabText}>Repositories</Text>
+        </Pressable>
+    </View>
+  );
+};
+
+export default AppBar;
